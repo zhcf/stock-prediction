8f6a580f7219cf0afdcf64e6d9bc38678da62109
zhcf <zhangchaofeng93@163.com>
2019-02-11 12:24:25 -0500

    add batch_up_down.py

---
+++ batch_up_down.py
@@ -0,0 +1,20 @@
+from data_preparation import get_stocks_data
+from const import *
+from stock_prediction_up_down import predict, train
+import sys
+
+if __name__ == '__main__':
+    stocks = get_stocks()
+    argv = sys.argv
+    action = "predict"
+    if len(argv) > 1:
+        action = argv[1]
+    if action == "predict":
+        print("===================predict====================")
+        get_stocks_data(stocks, get_predict_data=True)
+        predict(stocks)
+    elif action == "train":
+        print("===================train====================")
+        get_stocks_data(stocks, get_predict_data=False)
+        train(stocks)
+
