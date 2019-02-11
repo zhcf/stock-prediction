4918413ab0f9d785b0e9366a24f4500ea5b4b898
zhcf <zhangchaofeng93@163.com>
2019-02-11 10:06:22 -0500

    update the comments

--- const.py
+++ const.py
@@ -11,7 +11,26 @@ DIR_MODEL_SHORT_PARAMS = './model_short_params/'
 DIR_PREDICT_FULL_PARAMS = './predict_full_params/'
 DIR_PREDICT_SHORT_PARAMS = './predict_short_params/'
 
-USE_SHORT_PARAMS = True # use short params, if USE_SHORT_PARAMS= False, it  supports getting past 3 years data only, the training accuracy is not good.
+'''
+if USE_SHORT_PARAMS= True, the record columns will be the following, it supports getting all data, the training accuracy is good 
+date
+交易日期(index)
+open
+开盘价
+high
+最高价
+close
+收盘价
+low
+最低价
+volume
+成交量
+amount
+成交金额
+if USE_SHORT_PARAMS= False, the record columns will be the following, it supports getting past 3 years data only, the training accuracy is not good.
+属性:日期 ，开盘价， 最高价， 收盘价， 最低价， 成交量， 价格变动 ，涨跌幅，5日均价，10日均价，20日均价，5日均量，10日均量，20日均量，换手率
+'''
+USE_SHORT_PARAMS = True
 TRAINING_START_DATE = '1995-01-01'
 PREDICT_START_DATE = '2018-06-01'
 TARGET_COLUMN = 'close'
