41f451e2fa748bda5e5a557661794b12f388940a
zhcf <zhangchaofeng93@163.com>
2019-02-11 09:48:59 -0500

    support hyper parameter tuning

--- const.py
+++ const.py
@@ -11,7 +11,7 @@ DIR_MODEL_SHORT_PARAMS = './model_short_params/'
 DIR_PREDICT_FULL_PARAMS = './predict_full_params/'
 DIR_PREDICT_SHORT_PARAMS = './predict_short_params/'
 
-USE_SHORT_PARAMS = True
+USE_SHORT_PARAMS = True # use short params, if USE_SHORT_PARAMS= False, it  supports getting past 3 years data only, the training accuracy is not good.
 TRAINING_START_DATE = '1995-01-01'
 PREDICT_START_DATE = '2018-06-01'
 TARGET_COLUMN = 'close'
